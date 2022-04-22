6dfda29f0c8ff778fffcc9509b0f4e5662ff1652
Shishir Mehto <shishir.mehto@gmail.com>
2022-04-23 01:27:44 +0530

    added react chrome extension boiler plate

---
+++ src/pages/Popup/Popup.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../assets/img/logo.svg';
+import Greetings from '../../containers/Greetings/Greetings';
+import './Popup.css';
+
+const Popup = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>able to see the pop up and checking if the hot reload works</p>
+      </header>
+    </div>
+  );
+};
+
+export default Popup;
